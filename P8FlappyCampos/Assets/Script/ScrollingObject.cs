24d38013752514ae9d03bc0248208bcb55c3adce
CoolGus123 <gustavo4022010@gmail.com>
2024-12-10 09:58:07 -0800

    ive restarted the project
    ive restarted the project becuase i amde a mistaske thank god i found the error.

--- P8FlappyCampos/Assets/Script/ScrollingObject.cs
+++ P8FlappyCampos/Assets/Script/ScrollingObject.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrolllingObject : MonoBehaviour
+public class ScrollingObject : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0);
+        rb2d.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameControl.instance.gameOver = true)
+        if (GameControl.instance.gameOver == true)
         {
             rb2d.velocity = Vector2.zero;
         }
